32237f9504362d4369ab71e9b2419abc8e6ae380
Edgar Gutierrez <edgargu47@gmail.com>
2022-09-16 20:12:38 -0500

    HTTPS redirect with Talisman

--- index.py
+++ index.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+from flask_talisman import Talisman
 
 app = Flask(__name__)
 
 @app.route("/") #www.edgargtz.com
 def index():
     return render_template("index.html")
+
+# Wrap Flask app with Talisman
+Talisman(app, content_security_policy=None)
 
 if __name__ == "__main__":
     app.run(debug=True)
